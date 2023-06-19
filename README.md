bed6d270c89d5ae528f5295cbbfb7043d400a387
CoreBlender <CoreBlender@ethan_endures@outlook.com>
2023-06-19 09:55:42 -0400

    Community Plugins added

---
+++ README.md
@@ -0,0 +1,4 @@
+# Astronvim Config 
+
+## This is a user config file, suggested by neovim.
+## This allows me to configure neovim without conflicting changes when neovim updates
--- plugins/community.lua
+++ plugins/community.lua
@@ -4,6 +4,29 @@ return {
   -- example of imporing a plugin, comment out to use it or add your own
   -- available plugins can be found at https://github.com/AstroNvim/astrocommunity
 
-  -- { import = "astrocommunity.colorscheme.catppuccin" },
-  -- { import = "astrocommunity.completion.copilot-lua-cmp" },
+  -- ColorScheme: astrodark
+
+  -- Packs
+  { import = "astrocommunity.pack.rust" },
+  { import = "astrocommunity.pack.astro" },
+  { import = "astrocommunity.pack.bash" },
+  { import = "astrocommunity.pack.docker" },
+  { import = "astrocommunity.pack.go" },
+  { import = "astrocommunity.pack.haskell" },
+  { import = "astrocommunity.pack.html-css" },
+  { import = "astrocommunity.pack.json" },
+  { import = "astrocommunity.pack.lua" },
+  { import = "astrocommunity.pack.markdown" },
+  { import = "astrocommunity.pack.php" },
+  { import = "astrocommunity.pack.python" },
+  { import = "astrocommunity.pack.svelte" },
+  { import = "astrocommunity.pack.terraform" },
+  { import = "astrocommunity.pack.toml" },
+  { import = "astrocommunity.pack.typescript" },
+
+  -- Note Taking
+  { import = "astrocommunity.note-taking.obsidian-nvim" },
+
+  -- Completion
+  { import = "astrocommunity.completion.copilot-lua-cmp" },
 }
