3583ab5791f063564db993864747ad5c95a9bbf5
CoreBlender <117604672+CoreBlender@users.noreply.github.com>
2023-06-25 14:03:54 +0000

    Update community.lua

--- plugins/community.lua
+++ plugins/community.lua
@@ -7,27 +7,28 @@ return {
   -- ColorScheme: astrodark
 
   -- Packs
-  { import = "astrocommunity.pack.rust" },
-  { import = "astrocommunity.pack.astro" },
-  { import = "astrocommunity.pack.bash" },
-  { import = "astrocommunity.pack.docker" },
-  { import = "astrocommunity.pack.go" },
-  { import = "astrocommunity.pack.haskell" },
-  { import = "astrocommunity.pack.html-css" },
-  { import = "astrocommunity.pack.json" },
-  { import = "astrocommunity.pack.lua" },
-  { import = "astrocommunity.pack.markdown" },
-  { import = "astrocommunity.pack.php" },
-  { import = "astrocommunity.pack.python" },
-  { import = "astrocommunity.pack.svelte" },
-  { import = "astrocommunity.pack.terraform" },
-  { import = "astrocommunity.pack.toml" },
-  { import = "astrocommunity.pack.typescript" },
-  { import = "astrocommunity.pack.cmake" },
+  
+   -- { import = "astrocommunity.pack.rust" },
+   -- { import = "astrocommunity.pack.astro" },
+   -- { import = "astrocommunity.pack.bash" },
+   -- { import = "astrocommunity.pack.docker" },
+   -- { import = "astrocommunity.pack.go" },
+   -- { import = "astrocommunity.pack.haskell" },
+  --  { import = "astrocommunity.pack.html-css" },
+   -- { import = "astrocommunity.pack.json" },
+   -- { import = "astrocommunity.pack.lua" },
+   -- { import = "astrocommunity.pack.markdown" },
+  --  { import = "astrocommunity.pack.php" },
+  --  { import = "astrocommunity.pack.python" },
+   -- { import = "astrocommunity.pack.svelte" },
+ --   { import = "astrocommunity.pack.terraform" },
+  --  { import = "astrocommunity.pack.toml" },
+   -- { import = "astrocommunity.pack.typescript" },
+  --  { import = "astrocommunity.pack.cmake" },
 
   -- Note Taking
-  { import = "astrocommunity.note-taking.obsidian-nvim" },
+  --  { import = "astrocommunity.note-taking.obsidian-nvim" },
 
   -- Completion
-  { import = "astrocommunity.completion.copilot-lua-cmp" },
+   -- { import = "astrocommunity.completion.copilot-lua-cmp" },
 }
